c4aebf65a6ae2fc432649a9aa86869532ab8ac60
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-15 11:17:26 -0500

    front-end showing up

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var PORT = process.env.PORT || 3000;
 // app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: false
 }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({
@@ -28,7 +28,7 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + '/public/index.html')
 });
 
-app.listen(8888, function() {
+app.listen(3000, function() {
     console.log("App running on Port 3000");
 });
 // Solution 2: Customer Relations
