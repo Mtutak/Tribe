b406c8b01cd637bedd4e0d3224c892aaa7dc86f5
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-17 16:23:42 -0500

    userjob added...nothing rendering

--- app/components/children/UserPhoto.js
+++ app/components/children/UserPhoto.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 class UserPhoto extends React.Component {
     render() {
         return(
-        <div>
+        <div className="container">
             <div className="col-md-4 col-sm-6 padding-right-zero">
                 <div className="portfolio-box design">
                 <img src="img/port03.jpg" alt="" className="img-responsive" />
