374ce0c1efc64a82911311b51a6a593535f5287d
Matthew Tutak <mtutak91@gmail.com>
2017-03-18 17:00:08 -0500

    Fixed Reach Router Moved file directories to be together. Kept Form component name as Post Form Make sure to do localhost:8888/#/routename

--- app/app.js
+++ app/app.js
@@ -2,12 +2,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 //Router variable
-// import { router } from '../config/router';
+import { router } from './config/router';
 // import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import { Main } from '../app/components/Main';
-import { LandingPage } from './components/parents/LandingPage';
+
 //mounting point for index.html 
 const where = document.getElementById('app');
 
 
-ReactDOM.render(<LandingPage />, where);
+ReactDOM.render(router, where);
