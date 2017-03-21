3c1bad3e6a7a65279c5ce44601d5dbc575f2d788
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-21 09:40:56 -0500

    geo and autogeo created...for matts pull request

---
+++ app/components/children/form/AutoGeo.js
@@ -0,0 +1,23 @@
+//ES6 if need something in module need to import it
+//This module is to use HTML5 Geolocation to try and autopopulate the Geolocation
+//If the autopopulated location is not accurate or not supported then
+//display Geo component for autofill location
+
+import React from 'react';
+
+
+class AutoGeo extends React.Component {
+    //needs at least one method - render
+    render () {
+        return (
+            <div>
+                <h2> Auto-Geo </h2>
+                {/*nhttps://www.w3schools.com/html/html5_geolocation.asp */}
+            </div>
+        )
+    }
+}
+ 
+
+
+export { AutoGeo };
