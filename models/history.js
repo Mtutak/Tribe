fc6e4ea4e48c587429fbe022ffd48ed06c9038fd
Matthew Tutak <mtutak91@gmail.com>
2017-03-21 18:40:53 -0500

    history

--- models/history.js
+++ models/history.js
@@ -1,14 +1,36 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var HistorySchema = new Schema({
+var ProjectSchema = new Schema({
+  title: {
+    type: String
+  },
+  summary: {
+    type: String
+  },
+  detail: {
+    type: String
+  },
   location: {
     type: String
   },
+  latitude: {
+    type: Number
+  },
+  longitude: {
+    type: Number
+  },
   date: {
     type: Date
+  },
+  creator: {
+    type: String
+  },
+  team: {
+    type: Array
   }
+
 });
 
-var History = mongoose.model("History", HistorySchema);
-module.exports = History;
+var Project = mongoose.model("Project", ProjectSchema);
+module.exports = Project;
