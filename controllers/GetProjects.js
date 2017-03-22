045ea55020d5df5458b9fc10ab5f216d1db4c46a
Matthew Tutak <mtutak91@gmail.com>
2017-03-22 18:30:58 -0500

    Adding Mongoose and All Project Route mongoose models and all project route in controllers

---
+++ controllers/GetProjects.js
@@ -0,0 +1,26 @@
+const Project = require('../models/Project'),
+	  ProjectDetail = require('../models/ProjectDetail'),
+	  Connection = require('../models/Connection'),
+	  Profile = require('../models/Profile');
+
+exports.getProjects = function(req, res, next) {
+	var user
+	//creator is an array
+	Project.findAll({ creator: req.user._id})
+	.select('_id')
+	.exec(function(err, projects){
+		if (err) {
+			res.send({ error: err});
+			return next(err);
+		}
+		console.log(projects);
+		//go through each project id and populate detail
+		// projects.forEach(function(project) {
+		// 	ProjectDetail.find({'projectId': project._id})
+		// 	.sort('-createdAt')
+		// 	.populate({
+		// 		path: 'user'
+		// 	})
+		// })
+	})
+}
