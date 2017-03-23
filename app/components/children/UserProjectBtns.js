d16e458581f2fc89d661664953b49cf9de52f87e
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-22 19:52:02 -0500

    all projects button routing properly from profile

--- app/components/children/UserProjectBtns.js
+++ app/components/children/UserProjectBtns.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// import { Header } from '../children/Header';
+import { Link } from 'react-router';
 
 class UserProjectBtns extends React.Component {
     render() {
@@ -8,7 +8,7 @@ class UserProjectBtns extends React.Component {
                 <br />
                  <button className="button-medium my-projects-btn" id="contact-submit" type="submit" name="contact">My Projects</button>
                  <br />
-                 <button className="button-medium all-projects-btn" id="contact-submit" type="submit" name="contact">All Projects</button>
+                 <Link to="/projects"><button className="button-medium all-projects-btn" id="contact-submit" type="submit" name="contact">All Projects</button></Link>
             </div>
           
         );
