8b5427e197924afe5cc9315df1e49990ffc1003b
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-25 13:15:03 -0500

    modal showing

--- app/components/children/userprofbtn/ProfileButton.js
+++ app/components/children/userprofbtn/ProfileButton.js
@@ -1,47 +1,55 @@
 import * as React from 'react';
 import { ProfileForm } from './ProfileForm';
 import { notification } from 'antd';
+import { Modal } from 'react-bootstrap';
 import * as axios from 'axios'; // axios should be replaced with helpers
 import helpers from '../../utils/helpers';
 import Auth from '../../../modules/localAuth';
-
 class ProfileButton extends React.Component {
         handleClick(event) {
             event.preventDefault();
-            this.setState({formVisible: !this.state.formVisible});
+            // this.setState({formVisible: !this.state.formVisible});
+            this.setState({showModal: !this.state.showModal});
         }
        // Initial State
-
-        initializeState() {
-            this.setState({ formVisible: false});
+       initializeState() {
+           return { showModal: false };
+       }
+       constructor(props) {
+            super(props);
+            this.state = {
+                showModal: false
+            };
         }
-
+       close() {
+           this.setState({ showModal: false });
+       }
+       open() {
+           this.setState({ showModal:true });
+       }
+        // initializeState() {
+        //     this.setState({ formVisible: false});
+        // }
         // Lifecycle Methods
-
         componentWillMount() {
             this.initializeState();
         }
-
         sendSuccessNotification() {
             notification['success']({
             message: 'Yayyy!!',
             description: 'Your post has been created.',
             });
         }
-
         sendErrorNotification() {
             notification['error']({
             message: 'Uh Oh',
             description: 'Something went wrong, please try again.',
             });
         }
-
         // Data Request Methods
-
         postform(postObj) {
             this.startLoading();
             axios.post("/projects/new", { postObj });
-
             // helpers.postProject(postObj).then(() => {
             //     console.log('Post Form Success!');
             //     this.sendSuccessNotification();
@@ -58,7 +66,8 @@ class ProfileButton extends React.Component {
         return (
         <div>
            <center><button 
-                onClick={(event) => this.handleClick(event)} 
+                /*onClick={(event) => this.handleClick(event)} */
+                onClick={() => this.setState({ showModal: true})}
                 className="btn edit-me" 
                 id="userProfileForm" 
                 type="submit"
@@ -66,16 +75,26 @@ class ProfileButton extends React.Component {
             >
             Edit My Profile
             </button></center>
-            {
-            this.state.formVisible
+           {
+            <Modal show={this.state.showModal} onHide={this.close}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit your Profile</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                   ? <ProfileForm loading={this.state.loading} submitAction={(postObj) => this.postform(postObj)} />
+                   : null 
+                </Modal.Body>
+            </Modal>
+           }
+        </div>
+           /*{
+            /*this.state.formVisible*/
+            /*this.state.showModal
                 ? <ProfileForm loading={this.state.loading} submitAction={(postObj) => this.postform(postObj)}/>
                 : null
             }
-        </div>
-        )
+        </div>*/
+        );
     }
 }
-
-
-
 export  { ProfileButton };
